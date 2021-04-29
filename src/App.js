debd802641d3dc7b2b9113301646d946730bd3c7
mimimicek <33347700+MimiMicek@users.noreply.github.com>
2021-04-30 01:37:02 +0200

    second phase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
-import React, {useRef} from "react";
+import React, {useRef, useState, useEffect} from "react";
 import Webcam from "react-webcam";
+import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import { drawHand } from "./utilities";
-
+import * as fp from "fingerpose";
+import {loveGesture} from "./LoveGesture";
+//import victory from "./imgs/peace.png";
+import thumbs_up from "./imgs/ok.png";
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
   
+  //Set an emoji/image for detection
+  const [emoji, setEmoji] = useState(null);
+  const imgs = {thumbs_up:thumbs_up};
+
   //Loading in the Handpose model
   const getHandpose = async () => {
     const loadHandpose = await handpose.load();
-    console.log("HP model loaded");
+    //console.log("HP model loaded");
     setInterval(() => {
       detectHand(loadHandpose);
     }, 100);
@@ -22,26 +30,57 @@ function App() {
   const detectHand = async (loadHandpose) => {
       if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null &&
                  webcamRef.current.video.readyState === 4) {
-                  //Getting properties
-                  const video = webcamRef.current.video;
-                  const videoHeight = webcamRef.current.video.videoHeight;
-                  const videoWidth = webcamRef.current.video.videoWidth;
-                  
-                  //Setting video & canvas height and width
-                  webcamRef.current.video.height = videoHeight;
-                  webcamRef.current.video.width = videoWidth;
-                  canvasRef.current.height = videoHeight;
-                  canvasRef.current.width = videoWidth;
-                  
-                  //Detecting hand
-                  const hand = await loadHandpose.estimateHands(video);
-                  console.log(hand);
 
-                  //Drawing the net
-                  const ctx = canvasRef.current.getContext("2d");
-                  drawHand(hand, ctx);
-                  }
-                } 
+          //Getting properties
+          const video = webcamRef.current.video;
+          const videoHeight = webcamRef.current.video.videoHeight;
+          const videoWidth = webcamRef.current.video.videoWidth;
+          
+          //Setting video & canvas height and width
+          webcamRef.current.video.height = videoHeight;
+          webcamRef.current.video.width = videoWidth;
+          canvasRef.current.height = videoHeight;
+          canvasRef.current.width = videoWidth;
+          
+          //Detecting hand
+          const hand = await loadHandpose.estimateHands(video);
+          //console.log(hand);
+
+          //Detecting gestures and configuring custom gestures
+          if(hand.length > 0){
+              const GE = new fp.GestureEstimator([
+                fp.Gestures.VictoryGesture,
+                fp.Gestures.ThumbsUpGesture,
+                //loveGesture
+              ]);
+
+              //Setting up the gesture estimation
+              const gesture = await GE.estimate(hand[0].landmarks, 8);
+              console.log(gesture)
+
+              //Gesture should have high confidence in detection
+              if(gesture.gestures !== undefined && gesture.gestures.length > 0){
+                  console.log(gesture.gestures);
+
+                  //Grabbing the confidence of each suggested gesture
+                  const confidence = gesture.gestures.map(
+                    (prediction) => prediction.confidence
+                  );
+
+                  //Which gesture had the highest confidence
+                  const maxConfidence = confidence.indexOf(
+                    Math.max.apply(null, confidence)
+                  ); 
+
+                  setEmoji(gesture.gestures[maxConfidence].name);
+                  console.log(emoji);
+              }
+          }
+          //Drawing the hand net
+          const ctx = canvasRef.current.getContext("2d");
+          drawHand(hand, ctx);
+          }
+        } 
 
   getHandpose();
 
@@ -75,6 +114,20 @@ function App() {
                   zIndex:9
                 }}>
           </canvas>
+          {emoji !== null ? (
+          <img
+            src={imgs[emoji]}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 400,
+              bottom: 500,
+              right: 0,
+              textAlign: "center",
+              height: 100,
+            }}
+          />) : ("")}
       </header>
     </div>
   );
--- src/utilities.js
+++ src/utilities.js
@@ -7,72 +7,52 @@ const fingerJoints = {
     pinky: [0, 17, 18, 19, 20],
 };
 
-//Joint colours dictionary
-const style = {
-    0: { colour: "yellow", size: 13 },
-    1: { colour: "green", size: 5 },
-    2: { colour: "green", size: 10 },
-    3: { colour: "green", size: 5 },
-    4: { colour: "green", size: 5 },
-    5: { colour: "purple", size: 10 },
-    6: { colour: "purple", size: 5 },
-    7: { colour: "purple", size: 5 },
-    8: { colour: "purple", size: 5 },
-    9: { colour: "blue", size: 10 },
-    10: { colour: "blue", size: 5 },
-    11: { colour: "blue", size: 5 },
-    12: { colour: "blue", size: 5 },
-    13: { colour: "red", size: 10 },
-    14: { colour: "red", size: 5 },
-    15: { colour: "red", size: 5 },
-    16: { colour: "red", size: 5 },
-    17: { colour: "orange", size: 10 },
-    18: { colour: "orange", size: 5 },
-    19: { colour: "orange", size: 5 },
-    20: { colour: "orange", size: 5 },
-};
-  
 //Creating points in the hand
 export const drawHand = (predictions, ctx) => {
-    if(predictions.length > 0){
+    // Check if we have predictions
+    if (predictions.length > 0) {
+      // Loop through each prediction
         predictions.forEach((prediction) => {
-            const markers = prediction.markers;
-            
+            //Get markers
+            const landmarks = prediction.landmarks;
+    
+            // Loop through fingers
             for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
-                    let finger = Object.keys(fingerJoints)[j];
-                    //Looping through pairs of joints
-                    for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
-                    // Get pairs of joints
-                    const firstJointIndex = fingerJoints[finger][k];
-                    const secondJointIndex = fingerJoints[finger][k + 1];
-            
-                    // Draw path
-                    ctx.beginPath();
-                    ctx.moveTo(
-                        markers[firstJointIndex][0],
-                        markers[firstJointIndex][1]
-                    );
-                    ctx.lineTo(
-                        markers[secondJointIndex][0],
-                        markers[secondJointIndex][1]
-                    );
-                    ctx.strokeStyle = "plum";
-                    ctx.lineWidth = 4;
-                    ctx.stroke();
-                }
-              }
+            let finger = Object.keys(fingerJoints)[j];
+
+            //Loop through joint pairs
+            for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
+                //Get joint pairs
+                const firstJointIndex = fingerJoints[finger][k];
+                const secondJointIndex = fingerJoints[finger][k + 1];
+    
+                //Draw lines
+                ctx.beginPath();
+                ctx.moveTo(
+                landmarks[firstJointIndex][0],
+                landmarks[firstJointIndex][1]
+                );
+                ctx.lineTo(
+                landmarks[secondJointIndex][0],
+                landmarks[secondJointIndex][1]
+                );
+                ctx.strokeStyle = "black";
+                ctx.lineWidth = 4;
+                ctx.stroke();
+            }
+            }
 
             //Looping through & drawing the markers
-            for (let i = 0; i < markers.length; i++) {
+            for (let i = 0; i < landmarks.length; i++) {
                 //Getting the x & y points
-                const x = markers[i][0];
-                const y = markers[i][1];
+                const x = landmarks[i][0];
+                const y = landmarks[i][1];
                 //Start drawing
                 ctx.beginPath();
-                ctx.arc(x, y, style[i]["size"], 0, 3 * Math.PI);
+                ctx.arc(x, y, 5, 0, 3 * Math.PI);
         
                 //Set colour of dots
-                ctx.fillStyle = style[i]["colour"];
+                ctx.fillStyle = "white";
                 ctx.fill();
               }
         });
